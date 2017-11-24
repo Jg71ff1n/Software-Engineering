dd2a6ce31e1c846fd6d1defa7096496cf2d831b6
Joe Griffin <n0680192@ntu.ac.uk>
2017-11-24 18:35:56 +0000

    Added base code for prime seive and GCD functions, alongside basic implementation of pollard rhos algorithm

--- Task 3/primeFactors/headers/primeFactors.h
+++ Task 3/primeFactors/headers/primeFactors.h
@@ -6,6 +6,8 @@
 #define PRIMEFACTORS_NAS280317
 
 #include <list>
+#include <limits>
+#include "string.h"
 
 /*
 Compute the list of prime factors of the parameter.
@@ -13,6 +15,8 @@ The list should be sequenced in ascending order.
 E.g. primeFactors(18) should return {2,3,3}.
 If the parameter is 0 or 1, the resulting list should be empty.
 */
-std::list<unsigned long int> primeFactors(unsigned long int);
+std::list<unsigned long int> primeFactors(unsigned long int input);
+int greatestCommonDivisor(int x, int y);
+void seiveOfE();
 
 #endif
